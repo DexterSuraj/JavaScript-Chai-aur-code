abf7b141d33cf08335f354c20dccd136b5b2f4a2
DexterSuraj <147973493+DexterSuraj@users.noreply.github.com>
2024-10-18 14:52:54 +0000

    01_DataTypes For datattpes Basics

---
+++ Basics/01_DataTypes.js
@@ -0,0 +1,22 @@
+"use script";//Treat all code as newer version.
+//alert("")//we are using node js ,not browser.
+console.log(3+3); console.log("Hello")//its correct but not good Practice.
+//tc39.es is the original Official Website For JavaScript, But we can also preffer MDN.
+
+
+let Name="Suraj"
+let Age=22
+let isLogIn=false
+/*
+Data Types in JS
+1. Number
+2. bigint
+3. string
+4. boolean
+5. null= Standalone Value
+6. undefined= value not assigned
+7. symbol= unique
+
+*8. object
+*/
+console.log(typeof Age)
---
+++ Basics/Variables.js
@@ -0,0 +1,12 @@
+ const accountId=321654
+ let accountEmail="ashdbfkjsa@gmail.com"//It has scope{} this is scope.
+ var accountPassword="35441"//It dosent have scope{} this is scope.
+ accountCity="Ranchi";//It is a bad practice.
+let name;
+//accountId=2
+//console.log(accountId); constant cant be chang               
+  
+accountEmail="SAHDBF@gmail.com";
+accountPassword="4321";
+accountCity="Bokaro";
+console.log([accountEmail,accountId,accountPassword,accountCity,name]);
