ecd0fa72a8cb5fda08c9b4bbde69eda18fa1c288
DexterSuraj <147973493+DexterSuraj@users.noreply.github.com>
2024-10-19 07:57:06 +0000

    Operations and Pattern

--- Basics/02_Conversion.js
+++ Basics/02_Conversion.js
@@ -1,5 +1,71 @@
-let score=33
+/*
+
+let A="33"
+console.log(typeof A)
+console.log(typeof(A))
+let valueNumber = Number(A)
+console.log(typeof valueNumber)
+console.log(valueNumber+2)
+
+
+
+let B="33abc"
+console.log(typeof B)
+console.log(typeof(B))
+let InNumber = Number(B)
+console.log(typeof InNumber)
+console.log(InNumber+2)
+
+
+
+let c=22
+console.log(typeof c)
+console.log(typeof(c))
+let valueInString = Number(c)
+console.log(typeof valueInString)
+console.log(valueInString+2)
+
+
+
+let D=null
+console.log(typeof D)
+console.log(typeof(D))
+let valueInBoolean = Boolean(D)
+console.log(typeof valueInBoolean)
+console.log(valueInBoolean+2)
+
+
+
+
+let E=undefined
+console.log(typeof E)
+console.log(typeof(E))
+let Num = Number(E)
+console.log(typeof Num)
+console.log(Num+2)
+
+
+
+
+let score=true
 console.log(typeof score)
 console.log(typeof(score))
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber)
+console.log(valueInNumber+2)
+
+
+
+*/
+  ////{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{OPERATION}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}////
+
+const num1 =3;
+const num2 =2;
+console.log(num1**num2);
+//3*2   ==>6
+//3**2  ==>9 Squase
+//10%3  ==>1 Modulo (Remainder)
+ let str1="Hello";
+ let str2=" Hitesh";
+ let str3= str1+str2;
+ console.log(str3);
---
+++ shapes&patterns/triangle.js
@@ -0,0 +1,7 @@
+const num=5;
+for(let i=0;i<=5;i++){
+    for(let j=0;j<5;j++){
+        console.log("*")
+    }
+    console.log()
+}
